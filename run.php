ba1c0e07b1545cb5f9a3a58e1a6a33ca8c63215c
Jack Arthur Harrhy <me@jackharrhy.com>
2019-11-21 10:04:44 -0330

    exec time

--- run.php
+++ run.php
@@ -4,6 +4,8 @@ include __DIR__.'/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::create(__DIR__); $dotenv->load();
 
+$owner_id = getenv('PHAP_OWNER_ID');
+
 $discord = new \Discord\Discord([
   'token' => getenv('PHAP_DISCORD_TOKEN'),
 ]);
@@ -14,6 +16,14 @@ $discord->on('ready', function ($discord) {
   $discord->on('message', function ($message) {
     if (strpos($message->content, 'fap') === 0) {
       $message->reply('u mean phap');
+    }
+
+    if ($message->author->id === $GLOBALS['owner_id']) {
+      if (strpos($message->content, 'phapxecute ') === 0) {
+        $command = substr($message->content, strlen('phapxecute '));
+        $output = shell_exec($command.' 2>&1');
+        $message->channel->sendMessage('```'.$output.'```');
+      }
     }
   });
 });
