780a293690d99209c0bcb4fbfea0036e601035c5
Jack Arthur Harrhy <me@jackharrhy.com>
2024-11-14 14:38:14 -0330

    only annoy dani .5% of the time

--- phap.php
+++ phap.php
@@ -32,7 +32,7 @@ $discord->on('ready', function ($discord) {
       $message->reply('spit on that thang');
     }
 
-    if ($message->author->id === "480415224164253707") {
+    if ($message->author->id === "480415224164253707" && mt_rand(1, 10000) <= 50) {
       $message->channel->sendMessage('eleofant');
     }
 
